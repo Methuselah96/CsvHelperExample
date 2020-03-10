86c606fc549aef6e19b95e4b575b485ca09d2f6b
Nathan Bierema <nbierema@gmail.com>
2020-03-10 11:28:13 -0400

    Initial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using static System.Environment;
+using static System.Environment.SpecialFolder;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var records = new List<Foo>
+            {
+                new Foo { Id = 1, Name = "one" },
+            };
+
+            using (var writer = new StreamWriter(Path.Combine(GetFolderPath(DesktopDirectory), "file.csv")))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+    }
+
+    public class Foo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
